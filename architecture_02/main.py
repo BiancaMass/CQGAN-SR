350ad67cbb6aa04c3fef0c1175fe69bf8221cde1
BiancaMass <b.massacci@student.maastrichtuniversity.nl>
2023-11-27 21:18:30 +0100

    refactor: data set creation as stand-alone function

--- architecture_02/main.py
+++ architecture_02/main.py
@@ -1,14 +1,12 @@
 import config_a02
-from quantum_circuit import circuit
 from training import train_model
 from validation import validate_model
 from image_processing import train_test_image_generator, from_probs_to_image
-from visualizations import plot_images
-from cost_function import cost_fn
+from my_utils import create_dataset
 
 """
 Main training loop. Orchestrates the flow by calling functions from other scripts.
-Command-line interface handling if needed.
+Consider creating command-line interface handling if needed.
 """
 
 
@@ -26,56 +24,13 @@ def main(training_inputs, training_targets, val_inputs, val_targets, n_layers):
                                                                 validation_inputs=val_inputs,
                                                                 validation_targets=val_targets)
 
-    # # Call the circuit on another image # TODO: this should be done outside main()
-    # validation_input, validation_target = train_test_image_generator(
-    #                                             input_rows=config_a02.INPUT_ROWS,
-    #                                             input_cols=config_a02.INPUT_COLS,
-    #                                             output_rows=config_a02.OUTPUT_ROWS,
-    #                                             output_cols=config_a02.OUTPUT_COLS)
-    #
-    # probs_validation = circuit(params=trained_weights,
-    #                            flat_input_image=validation_input.flatten(),
-    #                            nr_layers=n_layers,
-    #                            destination_qubit_indexes=config_a02.DEST_QUBIT_INDEXES)
-    #
-    # validation_loss = cost_fn(params=trained_weights,
-    #                           original_image_flat=validation_input.flatten(),
-    #                           target_image_flat=validation_target.flatten(),
-    #                           nr_layers=n_layers,
-    #                           dest_qubit_indexes=config_a02.DEST_QUBIT_INDEXES)
-    #
-    # print("\n \n Validation loss is {:.4f}".format(validation_loss))
-    #
-    # validation_matrix = from_probs_to_image(probs_validation)
-    #
-    # plot_images([validation_input, validation_matrix],
-    #             ["Test image", "Output Test image"])
-
 
 images_per_training_set = config_a02.TRAINING_IMAGES_NUM
 images_per_validation_set = config_a02.VALIDATION_IMAGES_NUMBER
 
-# TODO: change this into a function
-train_images_inputs = []
-train_images_targets = []
-validation_images_inputs = []
-validation_images_targets = []
+train_images_inputs, train_images_targets = create_dataset(images_per_training_set)
+validation_images_inputs, validation_images_targets = create_dataset(images_per_validation_set)
 
-for i in range(images_per_training_set):
-    image_LR, image_HR = train_test_image_generator(input_rows=config_a02.INPUT_ROWS,
-                                                    input_cols=config_a02.INPUT_COLS,
-                                                    output_rows=config_a02.OUTPUT_ROWS,
-                                                    output_cols=config_a02.OUTPUT_COLS)
-    train_images_inputs.append(image_LR)
-    train_images_targets.append(image_HR)
-
-for i in range(images_per_validation_set):
-    image_LR, image_HR = train_test_image_generator(input_rows=config_a02.INPUT_ROWS,
-                                                    input_cols=config_a02.INPUT_COLS,
-                                                    output_rows=config_a02.OUTPUT_ROWS,
-                                                    output_cols=config_a02.OUTPUT_COLS)
-    validation_images_inputs.append(image_LR)
-    validation_images_targets.append(image_HR)
 
 if __name__ == "__main__":
     main(training_inputs=train_images_inputs,
--- architecture_02/my_utils.py
+++ architecture_02/my_utils.py
@@ -1,3 +1,6 @@
+from image_processing import train_test_image_generator
+import config_a02
+
 
 def destination_qubit_index_calculator(original_rows_num, original_cols_num):
     destination_cols = original_cols_num * 2 + 1
@@ -5,8 +8,24 @@ def destination_qubit_index_calculator(original_rows_num, original_cols_num):
     for r in range(1, original_rows_num + 1):
         for c in range(1, original_cols_num + 1):
             original_i, original_j = r, c
-            destination_i, destination_j = original_i*2, original_j*2
+            destination_i, destination_j = original_i * 2, original_j * 2
             destination_index = (destination_i - 1) * destination_cols + destination_j
             destination_qubit_indexes.append(destination_index)
 
     return destination_qubit_indexes
+
+
+# import config_a02
+def create_dataset(images_per_set):
+    images_inputs = []
+    images_targets = []
+
+    for i in range(images_per_set):
+        image_LR, image_HR = train_test_image_generator(input_rows=config_a02.INPUT_ROWS,
+                                                        input_cols=config_a02.INPUT_COLS,
+                                                        output_rows=config_a02.OUTPUT_ROWS,
+                                                        output_cols=config_a02.OUTPUT_COLS)
+        images_inputs.append(image_LR)
+        images_targets.append(image_HR)
+
+    return images_inputs, images_targets
