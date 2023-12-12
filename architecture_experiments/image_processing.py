d5fa6a508b62d51ac868ef5b55c14e151ec52015
BiancaMass <b.massacci@student.maastrichtuniversity.nl>
2023-12-12 17:41:48 +0100

    feat: new training image generation for more pixels

--- architecture_experiments/image_processing.py
+++ architecture_experiments/image_processing.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib.pyplot as plt
 import torch
 
 import config
@@ -64,6 +65,35 @@ def train_test_image_generator(input_rows: int,
 
     return image_LR, image_HR
 
+# TODO: still does not work perfectly but better
+
+def input_output_image_generator(nrows_in, ncols_in, nrows_out, ncols_out):
+    # Generate the training (input image)
+    input_image = np.ones((nrows_in, ncols_in))
+    black_pixel_row_in = np.random.randint(0, nrows_in)
+    black_pixel_col_in = np.random.randint(0, ncols_in)
+    input_image[black_pixel_row_in, black_pixel_col_in] = 0
+
+    # Calculate the relative position of the black pixel in the input image
+    relative_row = black_pixel_row_in / nrows_in
+    relative_col = black_pixel_col_in / ncols_in
+
+    # Scale the position to the output image dimensions
+    black_pixel_row_out = int(relative_row * nrows_out)
+    black_pixel_col_out = int(relative_col * ncols_out)
+
+    # Initialize the output image
+    output_image = np.ones((nrows_out, ncols_out))
+    max_dist = np.sqrt(max(black_pixel_row_out, nrows_out - 1 - black_pixel_row_out)**2 +
+                       max(black_pixel_col_out, ncols_out - 1 - black_pixel_col_out)**2)
+
+    for row in range(nrows_out):
+        for col in range(ncols_out):
+            dist = np.sqrt((row - black_pixel_row_out)**2 + (col - black_pixel_col_out)**2)
+            output_image[row, col] = dist / max_dist
+
+    return input_image, output_image
+
 
 def from_probs_to_image(probs_tensor):
 
@@ -76,4 +106,42 @@ def from_probs_to_image(probs_tensor):
     output_matrix = output_vector_subset.reshape(config.OUTPUT_ROWS, config.OUTPUT_COLS)
 
     return output_matrix
+
+
+
+# ERASE FROM HERE #
+
+# def plot_images(images, titles):
+#     """
+#     Plots a series of images in a single figure with individual subplots.
+#
+#     Parameters:
+#     - images (list): A list of images to be plotted.
+#     - titles (list): A list of titles for the subplots; should be the same length as images.
+#
+#     Each image is displayed in its own subplot with the corresponding title.
+#     """
+#     # The number of images
+#     num_images = len(images)
+#
+#     # Create a figure and a set of subplots
+#     fig, axs = plt.subplots(1, num_images, figsize=(5 * num_images, 5))
+#
+#     # If there's only one image, axs is not a list so we make it a list for consistency
+#     if num_images == 1:
+#         axs = [axs]
+#
+#     # Loop through each image and its corresponding title
+#     for ax, image, title in zip(axs, images, titles):
+#         ax.imshow(image, cmap='gray')
+#         ax.title.set_text(title)
+#         # ax.axis('off')  # Turn off axis
+#
+#     plt.tight_layout()  # Adjust subplots to fit into the figure area.
+#     plt.show()
+#
+#
+#
+# input, output = input_output_image_generator(3,3,6,6)
+# plot_images([input, output], ['in', 'out'])
 
