03b09b26ab2c95d10403c52c250903fd76a96701
BiancaMass <b.massacci@student.maastrichtuniversity.nl>
2024-01-09 18:23:36 +0100

    bug fixes

--- GAN/debug.py
+++ GAN/debug.py
@@ -2,6 +2,8 @@ import os
 
 import config
 
+from utils.circuit_utils import destination_qubit_index_calculator
+
 # Import the train function from your script
 # Make sure to replace 'your_script' with the actual name of your script file
 from train_qccc import train
@@ -10,17 +12,21 @@ def debug_train():
     # Set up debugging parameters
     layers = config.N_LAYERS  # number of layers in the generator
     n_data_qubits = config.N_QUBITS  # number of data qubits
+    img_size = config.INPUT_ROWS * config.INPUT_COLS
+    dest_qubit_indexes = destination_qubit_index_calculator(original_rows_num=config.INPUT_ROWS,
+                                                            original_cols_num=config.INPUT_COLS,
+                                                            scaling_factor=config.SCALING_FACTOR)
     batch_size = 8  # batch size for training
-    out_folder = config.OUTPUT_DIR  # output folder for saving results
+    # out_folder = config.OUTPUT_DIR  # output folder for saving results
 
     # Create the output directory if it doesn't exist
-    if not os.path.exists(out_folder):
-        os.makedirs(out_folder)
+    # if not os.path.exists(out_folder):
+    #     os.makedirs(out_folder)
 
     checkpoint = 0  # Start training from the beginning
 
     # Call the train function with the debugging parameters
-    train(layers, n_data_qubits, batch_size, out_folder, checkpoint)
+    train(layers, n_data_qubits, img_size, dest_qubit_indexes, batch_size, checkpoint)
 
 if __name__ == "__main__":
     debug_train()
